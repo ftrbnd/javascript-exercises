33968fad653f17ac54727bc247705a458c408d1c
ftrbnd <giosalas25@gmail.com>
2023-01-25 11:47:47 -0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, amt) {
+    if (amt < 0) return 'ERROR';
 
+    let res = '';
+    for (let i = 0; i < amt; i++)
+        res += str;
+
+    return res;
 };
 
 // Do not edit below this line
