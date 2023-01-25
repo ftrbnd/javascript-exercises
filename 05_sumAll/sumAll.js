f64c83258ca92d0dab26a6fd5c1cb56d8f9a6508
ftrbnd <giosalas25@gmail.com>
2023-01-25 12:13:33 -0800

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isFinite(start) || !Number.isFinite(end))
+        return 'ERROR';
 
+    if (start > end)
+        [start, end] = [end, start];
+
+    let sum = 0;
+    for (let i = start; i <= end; i++)
+        sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
