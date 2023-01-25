8ac5eb4fd845eb0190e8de9e7341f1320b2d3c1e
ftrbnd <giosalas25@gmail.com>
2023-01-25 12:05:18 -0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (const num of nums) {
+        if (arr.includes(num))
+            arr.splice(arr.indexOf(num), 1);
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
