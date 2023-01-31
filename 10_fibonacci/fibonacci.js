f1d687f58dc0c383b85bfad80a709d4a27e6a646
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-31 11:33:28 -0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    n = parseInt(n);
 
+    if (n == 1) return 1;
+
+    let first = 0, second = 1, fib = 0;
+    for (let i = 2; i <= n; i++) {
+        fib = first + second;
+        first = second;
+        second = fib;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
