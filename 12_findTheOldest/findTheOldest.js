71bdf9876666a51a73268be441b987e30e6e2c16
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-31 11:56:22 -0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    let oldestAge = 0, oldestPerson = {};
+    for (const person of people) {
+        let age;
+        if (!person.yearOfDeath) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        
+        oldestPerson = age > oldestAge ? person : oldestPerson;
+        oldestAge = Math.max(oldestAge, age);
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
