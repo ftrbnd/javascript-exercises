c2f23f28584e02f0b2153f130d7ba79adbada35b
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-31 11:13:59 -0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (!arr) return 0;
+  if (arr.length == 1) return arr[0];
+
+  let sum = 0;
+  
+  for (const num of arr) {
+    sum += num;
+  }
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 1;
+  
+  for (const num of arr) {
+    product *= num;
+  }
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) return 1;
+
+	let fact = num;
+  
+  for (let i = num - 1; i >= 1; i--) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
