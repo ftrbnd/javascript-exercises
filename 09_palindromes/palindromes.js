f2ad2a8b1ce0c6b883618a5cdd991e8b788775ed
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-31 11:21:54 -0800

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{ }=\-_`~()]/g,"").toLowerCase();
+    
+    let left = 0, right = str.length - 1;
+    while (left <= right) {
+        if (str[left++] != str[right--]) return false;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
